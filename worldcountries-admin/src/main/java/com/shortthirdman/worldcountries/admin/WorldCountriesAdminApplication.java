3b9746cacae1cb2579e91d7c47663170d0af9912
Swetank Mohanty <swetank.mohanty@outlook.com>
2021-08-02 15:48:41 +0530

    Added WebSecurity Configuration for HTTPS in Heroku

--- worldcountries-admin/src/main/java/com/shortthirdman/worldcountries/admin/WorldCountriesAdminApplication.java
+++ worldcountries-admin/src/main/java/com/shortthirdman/worldcountries/admin/WorldCountriesAdminApplication.java
@@ -1,13 +1,49 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.shortthirdman.worldcountries.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@SpringBootApplication
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.config.AdminServerHazelcastAutoConfiguration;
+
+/**
+ * @author shortthirdman-org
+ *
+ */
+@EnableAdminServer
+@SpringBootApplication(exclude = AdminServerHazelcastAutoConfiguration.class)
 public class WorldCountriesAdminApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorldCountriesAdminApplication.class, args);
 	}
+	
+	@Configuration
+	public static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	  @Override
+	  protected void configure(HttpSecurity http) throws Exception {
+		http.requiresChannel()
+		  .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+		  .requiresSecure();
+	  }
+	}
 }
